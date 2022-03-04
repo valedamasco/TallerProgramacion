6c91902526e67cfcd600b4d2366dc306c6340338
valedamasco <valedamasco11@gmail.com>
2022-03-04 00:11:22 -0300

    Add elements

--- includes/about-me.php
+++ includes/about-me.php
@@ -1,6 +1,20 @@
 <?php 
     include 'head.php';
     include 'header.php';
-    echo '<h1>Sobre mi</h1>';
+    var_dump($_SESSION)
+?>
+<div class="container">
+    <h1>Sobre mi</h1>
+    <table class="table" style="width: 40%">
+        <thead>
+            <tr> <td>Email</td> <td> <input type="email" name="email"> </td> </tr>
+            <tr> <td>Clave</td> <td> <input type="password" name="password"> </td> </tr>
+            <tr> <td>Alias</td> <td> <input type="text" name="alias"> </td> </tr>
+            <tr> <td><input type="submit"></td> </tr>
+
+        </thead>
+    </table>
+</div>
+<?php 
     include './footer.php';
 ?>
